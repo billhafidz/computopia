f562261af3f14a232bbf38996b55f07cdf51579c
Claris <kategawarin@gmail.com>
2024-05-04 23:23:08 +0700

    Revisi

--- manageUserAcc.php
+++ manageUserAcc.php
@@ -2,14 +2,29 @@
 session_start();
 include('server/connection.php');
 
-// Get user data
-$admin_name = $_SESSION['admin_name'];
+// Get user data from database
 $user_id = $_SESSION['user_id'];
-$user_name = $_SESSION['user_name'];
-$user_email = isset($_SESSION['user_email']) ? $_SESSION['user_email'] : 'N/A';
-$user_address = isset($_SESSION['user_address']) ? $_SESSION['user_address'] : 'N/A';
-$user_phone = isset($_SESSION['user_phone']) ? $_SESSION['user_phone'] : 'N/A';
-$user_photo = isset($_SESSION['user_photo']) ? $_SESSION['user_photo'] : 'N/A';
+$query = "SELECT * FROM users WHERE user_id = $user_id";
+$result = mysqli_query($conn, $query);
+if (!$result) {
+    die("Query failed: ". mysqli_error($conn));
+}
+
+$user_data = mysqli_fetch_assoc($result);
+
+// Set session variables
+$admin_name = $_SESSION['admin_name'] ?? $user_data['user_name'];
+$user_name = $user_data['user_name'];
+$user_email = $user_data['user_email'];
+$user_address = $user_data['user_address'];
+$user_phone = $user_data['user_phone'];
+$user_photo = $user_data['user_photo'];
+
+// Construct URL of user's photo
+$photo_url = 'images/' . $user_photo;
+
+// Close database connection
+mysqli_close($conn);
 ?>
 
 <body>
@@ -59,9 +74,11 @@ $user_photo = isset($_SESSION['user_photo']) ? $_SESSION['user_photo'] : 'N/A';
         <th type="subject">Image</th>
         <th>:</th>
         <td>
-          <?php echo htmlspecialchars($user_photo) ?>
+          <img style="width: 20%; height:20%" src="<?php echo htmlspecialchars($photo_url) ?>">
         </td>
       </tr>
     </table>
   </div>
+<a href="adminDashboard.php">
+<button type="button" class="btn btn-danger">Kembali</button>
 </body>
